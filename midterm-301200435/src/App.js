339d4881edbdba7b2edb6d312717cfe227cb4053
Ayowolemi Eni-omosule <ayowolemieniomosule@gmail.com>
2024-06-18 21:48:38 -0400

    I modified the app component

--- midterm-301200435/src/App.js
+++ midterm-301200435/src/App.js
@@ -1,13 +1,14 @@
 
 import React from 'react';
 import AddProduct from './components/AddProduct';
-
+import SignUpUser from './components/SignUpUser';
 
 function App() {
     return (
         <div className="App">
             <AddProduct />
-           
+            <h1>Sign Up User</h1>
+            <SignUpUser />
         </div>
     );
 }
