342b90dd23c5fd339c6f88dbf92a4b2ba18708db
Ayowolemi Eni-omosule <ayowolemieniomosule@gmail.com>
2024-06-18 20:53:24 -0400

    I modified the App component

--- midterm-301200435/src/App.js
+++ midterm-301200435/src/App.js
@@ -1,12 +1,10 @@
-// src/App.js
+
 import React from 'react';
 import AddProduct from './components/AddProduct';
 
 function App() {
     return (
         <div className="App">
-            <h1>MidTerm Project</h1>
-            <h2>Add Product</h2>
             <AddProduct />
         </div>
     );
