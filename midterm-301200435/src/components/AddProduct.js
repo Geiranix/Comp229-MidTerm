13bb68f183bdc3be7f89d979502adeb6749b9c77
Ayowolemi Eni-omosule <ayowolemieniomosule@gmail.com>
2024-06-18 20:57:47 -0400

    I made changes to the AddProduct component

--- midterm-301200435/src/components/AddProduct.js
+++ midterm-301200435/src/components/AddProduct.js
@@ -23,7 +23,7 @@ const AddProduct = () => {
     return (
         <form onSubmit={handleSubmit}>
             <div>
-                <label>Product Name</label>
+                <label> Name</label>
                 <input type="text" name="name" value={product.name} onChange={handleChange} />
             </div>
             <div>
@@ -31,14 +31,20 @@ const AddProduct = () => {
                 <input type="text" name="description" value={product.description} onChange={handleChange} />
             </div>
             <div>
-                <label>Price</label>
-                <input type="number" name="price" value={product.price} onChange={handleChange} />
+                <label>Category</label>
+                <input type="number" name="category" value={product.category} onChange={handleChange} />
             </div>
             <div>
                 <label>Quantity</label>
                 <input type="number" name="quantity" value={product.quantity} onChange={handleChange} />
             </div>
+            <div>
+                <label>Price</label>
+                <input type="number" name="price" value={product.price} onChange={handleChange} />
+            </div>
+           
             <button type="submit">Submit</button>
+            <button type="cancel">Cancel</button>
             
         </form>
     );
